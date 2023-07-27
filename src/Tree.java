d298f076861ccd9019339d791b8302fbbcca60f9
Overcat <malishavimukthi@gmail.com>
2023-07-28 00:04:40 +0530

    version 1

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,14 @@
+class TreeNode{
+    String data;
+    TreeNode child;
+    TreeNode sibling;
+
+    TreeNode(String data) {
+        this.data = data;
+        this.child = null;
+        this.sibling = null;
+    }
+}
+
 public class Tree {
 }
